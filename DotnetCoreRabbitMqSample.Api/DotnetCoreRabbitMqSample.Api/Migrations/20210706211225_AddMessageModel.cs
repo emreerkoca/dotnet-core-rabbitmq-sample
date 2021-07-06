3e5b016b46847bcc61f927088e5d2aab90534dab
Emre Erkoca <emreerkoca@outlook.com>
2021-07-07 00:16:32 +0300

    Created database for message entity

---
+++ DotnetCoreRabbitMqSample.Api/DotnetCoreRabbitMqSample.Api/Migrations/20210706211225_AddMessageModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotnetCoreRabbitMqSample.Api.Migrations
+{
+    public partial class AddMessageModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    Type = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true),
+                    ProcessTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Message");
+        }
+    }
+}
---
+++ DotnetCoreRabbitMqSample.Api/DotnetCoreRabbitMqSample.Api/Model/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DotnetCoreRabbitMqSample.Api.Model
+{
+    [Table("Message")]
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Type { get; set; }
+        public string Content { get; set; }
+        public DateTime ProcessTime { get; set; }
+    }
+}
